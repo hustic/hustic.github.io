fd15544f28931b50283e70da4b633e1ce9e1b811
sotos <sotgouli@gmail.com>
2018-05-30 14:10:08 +0300

    trying to add a list

--- js/app.js
+++ js/app.js
@@ -1,13 +1,47 @@
-var app = angular.module('hustic', []);
+var app = angular.module('hustic', ['ngAnimate']);
 
 app.controller('particlesCtrl', ['$scope', particlesCtrl]);
 app.directive('particlesDrv', ['$window', '$log', particlesDrv]);
 
+app.controller('repeatController', function($scope) {
+  $scope.friends = [
+    {name:'John', age:25, gender:'boy'},
+    {name:'Jessie', age:30, gender:'girl'},
+    {name:'Johanna', age:28, gender:'girl'},
+    {name:'Joy', age:15, gender:'girl'},
+    {name:'Mary', age:28, gender:'girl'},
+    {name:'Peter', age:95, gender:'boy'},
+    {name:'Sebastian', age:50, gender:'boy'},
+    {name:'Erika', age:27, gender:'girl'},
+    {name:'Patrick', age:40, gender:'boy'},
+    {name:'Samantha', age:60, gender:'girl'}
+  ];
+});
 
 
-function particlesCtrl($scope) {
-  $scope.showParticles = true;
-}
+
+var friends = element.all(by.repeater('friend in friends'));
+
+it('should render initial data set', function() {
+  expect(friends.count()).toBe(10);
+  expect(friends.get(0).getText()).toEqual('[1] John who is 25 years old.');
+  expect(friends.get(1).getText()).toEqual('[2] Jessie who is 30 years old.');
+  expect(friends.last().getText()).toEqual('[10] Samantha who is 60 years old.');
+  expect(element(by.binding('friends.length')).getText())
+      .toMatch("I have 10 friends. They are:");
+});
+
+ it('should update repeater when filter predicate changes', function() {
+   expect(friends.count()).toBe(10);
+
+   element(by.model('q')).sendKeys('ma');
+
+   expect(friends.count()).toBe(2);
+   expect(friends.get(0).getText()).toEqual('[1] Mary who is 28 years old.');
+   expect(friends.last().getText()).toEqual('[2] Samantha who is 60 years old.');
+ });
+
+
 
 function menu() {
     var x = document.getElementById("menu");
@@ -20,6 +54,13 @@ function menu() {
         y.style.backgroundImage = "url('./assets/logo.png')";
     }
 } 
+
+
+/* ==========================================================================*/
+
+function particlesCtrl($scope) {
+  $scope.showParticles = true;
+}
 
 function particlesDrv($window, $log) {
   return {
